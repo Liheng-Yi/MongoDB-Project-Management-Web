835d310cddabe212e36940bfcdd4b47e83f33d8a
Excalibur0331 <eli1838459978@gmail.com>
2023-09-20 16:57:13 -0700

    finished template

--- db/dbConnector_Sqlite.js
+++ db/dbConnector_Sqlite.js
@@ -1,24 +1,38 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':./bikeShare.sqlite');
 const {open} = require("sqlite");
+
+const db = new sqlite3.Database("./db/bikeShare.sqlite3");
+
 
 
 
 
 async function connect() {
     return open({
-        filename = 
-    }
+        filename:  "./db/bikeShare.sqlite3",
+        driver: sqlite3.Database,
+    });
+}
 
-    )
-    db.all(`SELECT ride_id,start_station_name,end_station_name 
-FROM tripdata
-ORDER BY ride_id DESC
-LIMIT 20;
-`);
+async function getTripData() {
+    const db = await connect()
+    const tripdata = 
+        await db.all(`SELECT ride_id,start_station_name,end_station_name 
+        FROM tripdata
+        ORDER BY ride_id DESC
+        LIMIT 4;
+        `);
+        console.log("db connector got the data")
 
 
-});
+    return tripdata
+}
 
-db.close();
+
+
+module.exports = {
+    getTripData
+}
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+const { getTripData} = require("../db/dbConnector_Sqlite.js")
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SF BikeShare Riders' });
+router.get('/', async function(req, res, next) {
+
+  const tripdata = await getTripData();
+
+  res.render('index', { title: 'SF BikeShare Riders!', tripdata: tripdata});
+
+
+
 });
 
 module.exports = router;
