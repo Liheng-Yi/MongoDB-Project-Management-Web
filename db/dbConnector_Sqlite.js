2c9daa28a6d9ed9122ee224d39089666f2936853
Excalibur0331 <eli1838459978@gmail.com>
2023-12-03 23:56:00 -0800

    finished mongoDB

--- db/dbConnector_Sqlite.js
+++ db/dbConnector_Sqlite.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const {open} = require("sqlite");
 
 
-async function connect() {
+async function  () {
     const db = await open({
         filename: "./db/company.sqlite3",
         driver: sqlite3.Database
@@ -20,7 +20,7 @@ async function getTripData() {
         // FROM Clients
         // ORDER BY clientID DESC;
         // `);
-        
+
     return tripdata
 }
 
@@ -46,156 +46,12 @@ async function getProjectData() {
 }
 
 
-async function addClient(sql, name, email, req, res) {
-    try {
-        const db = await connect();
-        await db.run(sql, [name, email], (err) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Failed to add client");
-                return;
-            }
-            else res.status(200).send("Client added successfully");
-        });
-        db.close();
-    } catch (error) {
-        console.error("An error occurred:", error);
-        res.status(500).send("Server error");
-    }
-}
-async function addAddress(sql, address, cID, req, res) {
-    try {
-        const db = await connect();
-                // Check if the client with the given cID exists
-        const clientExists = await db.get("SELECT clientID FROM Clients WHERE clientID = ?", [cID]);
-        if (!clientExists) {
-            res.status(400).send("Error adding the address: Client with the given ID does not exist");
-            return "handled";
-        }
-        await db.run(sql, [address, cID], (err) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Failed to add address");
-                return;
-            }
-            res.status(200).send("Address added successfully");
-        });
-        db.close();
-    } catch (error) {
-        console.error("An error occurred:", error);
-        res.status(500).send("Server error");
-    }
-}
-
-async function addProject(sql, name, aID, req, res) {
-    try {
-        const db = await connect();
-        // Check if the address with the given aID exists
-        const addressExists = await db.get("SELECT addressID FROM Addresses WHERE addressID = ?", [aID]);
-        if (!addressExists) {
-            res.status(400).send("Error adding the address: Client with the given ID does not exist");
-            return "handled";
-        }
-        await db.run(sql, [name, aID], (err) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Failed to add project");
-                return;
-            }
-            res.status(200).send("Project added successfully");
-        });
-        db.close();
-    } catch (error) {
-        console.error("An error occurred:", error);
-        res.status(500).send("Server error");
-    }
-}
-
-
-
-async function modifyClient(sql, name, email, clientID, req, res) {
-    try {
-        const db = await connect();
-        await db.run(sql, [name, email, clientID], (err) => {  // Note the order of parameters
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Failed to modify client");
-                return;
-            }
-            res.status(200).send("Client modified successfully");
-        });
-        db.close();
-    } catch (error) {
-        console.error("An error occurred:", error);
-        res.status(500).send("Server error");
-    }
-}
-
-
-
-const deleteClient = async (clientId) => {
-    const db = await connect();
-    
-    // Start a database transaction
-    try {
-        sql = 'DELETE FROM Clients WHERE clientID = ?';
-        await db.run(sql, [clientId]);
-    } catch (err) {
-        await db.run('ROLLBACK');
-        res.status(500).send("Server error");
-    }
-    await db.close();
-    
-}
-
-const deleteAddress = async (addressId) => {
-    const db = await connect();
-     try {
-        let sql = 'DELETE FROM Addresses WHERE addressID = ?';
-        await db.run(sql, [addressId]);
-    } catch (err) {
-        console.error("An error occurred:", error);
-        res.status(500).send("Server error");
-    } 
-    await db.close();
-}
-const deleteProject = async (projectId) => {
-    const db = await connect();
-    try {
-        let sql = 'DELETE FROM Projects WHERE projectId = ?';
-        await db.run(sql, [projectId]);
-    } catch (err) {
-        console.error("An error occurred:", err);
-        throw err;
-    } 
-    await db.close();
-}
-
-const modifyAddress = async (sql, address, cID, addressID) => {
-    const db = await connect();
-    try {
-        const clientExists = await db.get("SELECT clientID FROM Clients WHERE clientID = ?", [cID]);
-        if (!clientExists) {
-            return "client not found";
-        }
-
-        await db.run(sql, [address, cID, addressID]);
-        db.close();
-        return "handled";
-    } catch (error) {
-        console.error("An error occurred:", error);
-        throw error; // propagate the error to be handled in the route
-    }
-}
 
 
 
 
 module.exports = {
-    getTripData, getAddressData, getProjectData,
-    addClient, addProject, addAddress,
-    deleteClient, deleteProject, deleteAddress,
-    modifyClient, modifyAddress
+    getTripData, getAddressData, getProjectData
 }
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,183 +5,124 @@ const path = require('path');
 
 
 const {
-    getTripData, getAddressData, getProjectData,
-    addClient, addProject, addAddress,
-    deleteClient, deleteProject, deleteAddress,
-    modifyClient, modifyAddress
+    getClientData, projectsListWithEmployeeNames, addClient, deleteClient, editClient,
+    addProject, deleteProject, editProject
     
-} = require("../db/dbConnector_Sqlite.js");
+} = require("../db/mongoDBConnector.js");
 
 router.get('/', async function(req, res, next) {
-
-  const tripdata = await getTripData();
-
-  res.render('client', { title: 'Clients', tripdata: tripdata});
+  const clientData = await getClientData();
+  console.log(clientData)
+  res.render('client', { title: 'Clients', clientData: clientData});
 
 });
 
 /* GET home page. */
 router.get('/client', async function(req, res, next) {
 
-  const tripdata = await getTripData();
-
-  res.render('client', { title: 'Clients', tripdata: tripdata});
+  const clientData = await getClientData();
+  res.render('client', { title: 'Clients', clientData: clientData});
 
 });
 
-router.get('/address', async function(req, res, next) {
-
-  const addressData = await getAddressData();
-  // console.log("index.js: here is address data", addressData)
-  res.render('address', { title: 'Address', tripdata: addressData});
-});
 
 router.get('/project', async function(req, res, next) {
 
-  const projectData = await getProjectData();
-  // console.log("index.js: here is address data", addressData)
-  res.render('project', { title: 'Project', tripdata: projectData});
+  const clientData = await projectsListWithEmployeeNames();
+
+  
+  res.render('project', { title: 'Project', clientData: clientData});
+  
 });
 
 
 router.post('/clients', async function(req, res) {
-    const { name, email } = req.body;
-    const sql = 'INSERT INTO Clients(name, email) VALUES(?, ?)';
-    await addClient(sql, name, email, req, res);
-    res.redirect('/client')
-});
+    const { name, email, address } = req.body;
 
-router.post('/addresses', async function(req, res) {
-    const { address, cID } = req.body;
-    const sql = 'INSERT INTO Addresses(address, cID) VALUES(?, ?)';
-    
+    // Create a new client object
+    const newClient = {
+        client_name: name,
+        email: email,
+        address: address
+    };
+
     try {
-        const result = await addAddress(sql, address, cID, req, res);
-        if (result !== 'handled') {
-            res.redirect('/address');
-        }
-    } catch (error) {
-        console.error("An error occurred:", error);
-        if (!res.headersSent) {
-            // generic error 
-            res.status(500).send("Server error");
-        }
+        const result = await addClient(newClient);
+        res.status(201).json(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Internal Server Error');
     }
 });
 
+
+
 router.post('/projects', async function(req, res) {
-    const { name, aID } = req.body; // Extract project name and address ID from the request
-    const sql = 'INSERT INTO Projects(name, aID) VALUES(?, ?)'; // SQL statement to insert into the Projects table
-    console.log(req.body)
+
+    const { name, aID, eID } = req.body;
+
+    const newClient = {
+        name: name,
+        clientId: aID,
+        employeeId: eID
+    };
+    
     try {
-        // Assuming you have a function to add a project, similar to addAddress
-        const result = await addProject(sql, name, aID, req, res);
-        if (result !== 'handled') {
-            res.redirect('/project'); // Redirect to the project page after successful insertion
-        }
-    } catch (error) {
-        console.error("An error occurred:", error);
-        if (!res.headersSent) {
-            res.status(500).send("Server error");
-        }
+        const result = await addProject(newClient);
+        res.status(201).json(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Internal Server Error');
     }
 });
 
 router.delete('/clients/:id', async function(req, res) {
-    const clientId = req.params.id;
+
+    const clientID = req.params.id;
     try {
-        await deleteClient(clientId);
-        // Successfully deleted the client and its addresses
-        res.status(200).send("Client and associated addresses deleted successfully");
-    } catch (error) {
-        // Handle the error
-        console.error("Failed to delete client and associated addresses:", error);
-        res.status(500).send("Internal Server Error");
+        const result = await deleteClient(clientID);
+        res.status(201).json(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Internal Server Error');
     }
+
 });
 
-router.delete('/address/:id', async function(req, res) {
-    const addressId = req.params.id;
-    try {
-        await deleteAddress(addressId);
-        // Successfully deleted the client and its addresses
-        res.status(200).send("Client and associated addresses deleted successfully");
-    } catch (error) {
-        // Handle the error
-        console.error("Failed to delete client and associated addresses:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
 
 router.delete('/projects/:id', async function(req, res) {
-    const projectId = req.params.id;
-    console.log(projectId)
-    try {
-        await deleteProject(projectId);
-        // Successfully deleted the project
-        res.status(200).send("Project deleted successfully");
-    } catch (error) {
-        // Handle the error
-        console.error("Failed to delete project:", error);
-        res.status(500).send("Internal Server Error");
-    }
+    console.log("111")
 });
+
 
 router.patch('/clients/:id', async function(req, res) {
-    const { name, email } = req.body;
-    const clientID = req.params.id;  // Extracting the client ID from the URL
-    const sql = 'UPDATE Clients SET name = ?, email = ? WHERE clientID = ?';
-    try{
-      await modifyClient(sql, name, email, clientID, req, res);
-      res.status(200).send("Client and associated addresses deleted successfully");
-    } catch (error) {
-        // Handle the error
-        console.error("Failed to edit client:", error);
-        res.status(500).send("Internal Server Error");
-    }
-    
-});
-
-router.patch('/addresses/:id', async function(req, res) {
-    const { address, cID } = req.body;
-    const addressID = req.params.id;
-    const sql = `UPDATE addresses SET address = ?, cID = ? WHERE addressID = ?`;
-
+    const clientID = req.params.id;
+    const clientData = req.body;
     try {
-        const result = await modifyAddress(sql, address, cID, addressID);
-        if (result === 'handled') {
-            res.status(200).send("Address edited successfully");
-        }
-        else{
-            res.redirect('/address');
-        }
-    } catch (error) {
-        console.error("Failed to edit address:", error);
-        res.status(500).send("Internal Server Error");
+        const result = await editClient(clientID, clientData);
+        res.status(200).json({ message: "Client updated successfully", data: result });
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Internal Server Error');
     }
 });
 
-router.post('/reset', async function(req, res) {
-    // Construct the path to the .sql file
-    const sqlFilePath = path.join(__dirname, '/sql/testing.sql');
-    console.log("!!!")
+
+router.patch('/projects/:id', async function(req, res) {
+    const projectID = req.params.id;
+    const projectData = req.body;
+    console.log("!!!!!")
+    console.log(projectID)
+    console.log(projectData)
     try {
-        // Read the file's content
-        const sql = await fs.readFile(sqlFilePath, 'utf8');
-        console.log(sql)
-        // Pass the SQL file content to the reset function
-        const result = await reset(sql);
-
-        if (result === 'handled') {
-            res.status(200).send("Reset successfully");
-        } else {
-            res.redirect('/client');
-        }
-    } catch (error) {
-        console.error("Failed to reset:", error);
-        res.status(500).send("Internal Server Error");
+        const result = await editProject(projectID, projectData);
+        res.status(200).json({ message: "Project updated successfully", data: result });
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Internal Server Error');
     }
 });
+
 
 
 
